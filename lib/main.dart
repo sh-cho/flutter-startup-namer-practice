41e174e3c9782d9184b63951c7029c8c80a2f704
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-11-08 00:31:07 +0900

    Set theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
       home: RandomWords(),
     );
   }
